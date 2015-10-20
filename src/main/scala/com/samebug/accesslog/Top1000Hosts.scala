9fa4ff6f87d92dd7eb62031ed7d463156576c26f
Arpad Tamasi <arpad.tamasi@samebug.io>
2015-10-20 09:53:54 +0200

    count false negatives

--- src/main/scala/com/samebug/accesslog/Top1000Hosts.scala
+++ src/main/scala/com/samebug/accesslog/Top1000Hosts.scala
@@ -14,7 +14,7 @@ object Top1000Hosts {
   def execute(master: Option[String], args: Seq[String], jars: Seq[String] = Nil) {
     val sc = initContext(master, jars)
     try {
-      val file = sc.textFile(args.head)
+      val file: RDD[String] = sc.textFile(args.head)
       val records: RDD[CombinedLogRecord] = file.collect(CombinedLogRecord.parseRecord)
 
       top1000Hosts(file.count(), records)
@@ -25,12 +25,11 @@ object Top1000Hosts {
 
   def top1000Hosts(numberOfLines: Long, records: RDD[CombinedLogRecord]): Unit = {
     val clients: RDD[(String, CombinedLogRecord)] = records map { r => r.clientIp -> r }
-    val numberOfPageViewsPerClient = clients.aggregateByKey(0)({case(pageViews, _) =>  pageViews + 1}, { _ + _ })
+    val numberOfPageViewsPerClient: RDD[(String, Int)] = clients.aggregateByKey(0)({case(pageViews, _) =>  pageViews + 1}, { _ + _ })
     val clientsSortedByPageViewsDescending = numberOfPageViewsPerClient.sortBy(-_._2)
-
+    clientsSortedByPageViewsDescending.saveAsTextFile("clients.txt")
     val top1000 = clientsSortedByPageViewsDescending.take(100)
     val top1000Hosts = top1000 map { case (ip, pageViews) => hostName(ip) -> pageViews }
-
     println(s"Top ${top1000Hosts.size } clients collected from ${records.count() } records, $numberOfLines log rows")
     top1000Hosts foreach { case (host, visits) => println(s"$visits\t$host") }
   }
